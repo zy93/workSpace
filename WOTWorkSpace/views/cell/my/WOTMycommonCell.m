acb0c4cbfa4e91629466e77e7e73ef183cb2b596
sisi0528 <1211355668@qq.com>
2017-07-04 13:16:35 +0800

    +提交丢失文件

--- WOTWorkSpace/views/cell/my/WOTMycommonCell.m
+++ WOTWorkSpace/views/cell/my/WOTMycommonCell.m
@@ -41,8 +41,8 @@
         
         maker.trailingMargin.mas_equalTo(self.contentView).offset(-8);
         maker.centerY.mas_equalTo(self.contentView);
-        maker.height.mas_equalTo(15);
-        maker.width.mas_equalTo(20);
+        maker.height.mas_equalTo(13);
+        maker.width.mas_equalTo(7);
     }];
     
     [_lineview mas_makeConstraints:^(MASConstraintMaker *maker){
