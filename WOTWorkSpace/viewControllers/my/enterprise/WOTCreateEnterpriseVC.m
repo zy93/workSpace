15f567fef4fdd6f40c6725e9eb67178b8e28abf8
sisi0528 <1211355668@qq.com>
2017-07-07 10:06:09 +0800

    +第一次解决冲突

--- WOTWorkSpace/viewControllers/my/enterprise/WOTCreateEnterpriseVC.m
+++ WOTWorkSpace/viewControllers/my/enterprise/WOTCreateEnterpriseVC.m
@@ -1,51 +1,41 @@
-////
-////  WOTCreateEnterpriseVC.m
-////  WOTWorkSpace
-////
-////  Created by 张姝枫 on 2017/6/29.
-////  Copyright © 2017年 张姝枫. All rights reserved.
-////
 //
-//#import "WOTCreateEnterpriseVC.h"
+//  WOTCreateEnterpriseVC.m
+//  WOTWorkSpace
 //
-//@interface WOTCreateEnterpriseVC ()
+//  Created by 张姝枫 on 2017/6/29.
+//  Copyright © 2017年 张姝枫. All rights reserved.
 //
-//@end
-//
-//@implementation WOTCreateEnterpriseVC
-//
-//- (void)viewDidLoad {
-//    [super viewDidLoad];
-//    
-//    // Do any additional setup after loading the view.
-//}
-//
-//- (void)didReceiveMemoryWarning {
-//    [super didReceiveMemoryWarning];
-//    // Dispose of any resources that can be recreated.
-//}
-//
-//
-//-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-//    return 70;
-//}
-//-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-//    WOTMyEnterPriseCell *enterprisecell = [tableView dequeueReusableCellWithIdentifier:@"myenterpriseCellID" forIndexPath:indexPath];
-//    enterprisecell.enterpariseName.text = @"北京物联港科技发展有限公司";
-//    enterprisecell.joinEnterpriseTime.text = @"2017-06-12";
-//    enterprisecell.enterpriseHeaderImage.image = [UIImage imageNamed:@"myenterprise_logo"];
-//    return enterprisecell;
-//}
-//
-//
-///*
-//#pragma mark - Navigation
-//
-//// In a storyboard-based application, you will often want to do a little preparation before navigation
-//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-//    // Get the new view controller using [segue destinationViewController].
-//    // Pass the selected object to the new view controller.
-//}
-//*/
-//
-//@end
+
+#import "WOTCreateEnterpriseVC.h"
+
+@interface WOTCreateEnterpriseVC ()
+
+@end
+
+@implementation WOTCreateEnterpriseVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
