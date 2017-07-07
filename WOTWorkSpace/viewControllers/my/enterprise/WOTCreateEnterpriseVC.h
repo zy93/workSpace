69c0b1025737786f17a442321719cfc3fb2ce751
sisi0528 <1211355668@qq.com>
2017-07-07 09:38:33 +0800

    +代码提交

--- WOTWorkSpace/viewControllers/my/enterprise/WOTCreateEnterpriseVC.h
+++ WOTWorkSpace/viewControllers/my/enterprise/WOTCreateEnterpriseVC.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface WOTCreateEnterpriseVC : UIViewController
+#import "WOTTableViewBaseVC.h"
+@interface WOTCreateEnterpriseVC : WOTTableViewBaseVC
 
 @end
--- WOTWorkSpace/viewControllers/my/enterprise/WOTCreateEnterpriseVC.m
+++ WOTWorkSpace/viewControllers/my/enterprise/WOTCreateEnterpriseVC.m
@@ -1,37 +1,51 @@
+////
+////  WOTCreateEnterpriseVC.m
+////  WOTWorkSpace
+////
+////  Created by 张姝枫 on 2017/6/29.
+////  Copyright © 2017年 张姝枫. All rights reserved.
+////
 //
-//  WOTCreateEnterpriseVC.m
-//  WOTWorkSpace
+//#import "WOTCreateEnterpriseVC.h"
 //
-//  Created by 张姝枫 on 2017/6/29.
-//  Copyright © 2017年 张姝枫. All rights reserved.
+//@interface WOTCreateEnterpriseVC ()
 //
-
-#import "WOTCreateEnterpriseVC.h"
-
-@interface WOTCreateEnterpriseVC ()
-
-@end
-
-@implementation WOTCreateEnterpriseVC
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
-@end
+//@end
+//
+//@implementation WOTCreateEnterpriseVC
+//
+//- (void)viewDidLoad {
+//    [super viewDidLoad];
+//    
+//    // Do any additional setup after loading the view.
+//}
+//
+//- (void)didReceiveMemoryWarning {
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
+//
+//
+//-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+//    return 70;
+//}
+//-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+//    WOTMyEnterPriseCell *enterprisecell = [tableView dequeueReusableCellWithIdentifier:@"myenterpriseCellID" forIndexPath:indexPath];
+//    enterprisecell.enterpariseName.text = @"北京物联港科技发展有限公司";
+//    enterprisecell.joinEnterpriseTime.text = @"2017-06-12";
+//    enterprisecell.enterpriseHeaderImage.image = [UIImage imageNamed:@"myenterprise_logo"];
+//    return enterprisecell;
+//}
+//
+//
+///*
+//#pragma mark - Navigation
+//
+//// In a storyboard-based application, you will often want to do a little preparation before navigation
+//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+//    // Get the new view controller using [segue destinationViewController].
+//    // Pass the selected object to the new view controller.
+//}
+//*/
+//
+//@end
