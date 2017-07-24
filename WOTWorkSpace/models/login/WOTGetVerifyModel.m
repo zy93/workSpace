355419baec8023e5d53b694e9a0a206a9ea001f5
张雨 <zhangyu@blackcrystaltech.com>
2017-07-24 18:40:19 +0800

    +修改

---
+++ WOTWorkSpace/models/login/WOTGetVerifyModel.m
@@ -0,0 +1,13 @@
+//
+//  WOTGetVerifyModel.m
+//  WOTWorkSpace
+//
+//  Created by 张雨 on 2017/7/24.
+//  Copyright © 2017年 北京物联港科技发展有限公司. All rights reserved.
+//
+
+#import "WOTGetVerifyModel.h"
+
+@implementation WOTGetVerifyModel
+
+@end
