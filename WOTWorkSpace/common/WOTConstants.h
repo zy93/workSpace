0e01eec58ba2ea03e40f11e77a104b89a1ae648f
张雨 <zhangyu@blackcrystaltech.com>
2017-08-04 18:02:49 +0800

    +URL问题

--- WOTWorkSpace/common/WOTConstants.h
+++ WOTWorkSpace/common/WOTConstants.h
@@ -70,8 +70,7 @@
 //#define HTTPBaseURL @"http://www.yiliangang.net:8012/workSpace"
 #define HTTPBaseURL @"http://192.168.1.216:8080/workSpace"
 //#define HTTPBaseURL @"http://192.168.6.166/workSpace"
-#define HTTPBaseURL @"http://www.yiliangang.net:8012/workSpace"
 //#define HTTPBaseURL @"http://192.168.6.219:8080/workSpace"
-//#define HTTPBaseURL @"http://192.168.6.219:8080/workSpace"
+
 #define LOGIN_STATE_USERDEFAULT @"islogin"
 #endif /* WOTConstants_h */
