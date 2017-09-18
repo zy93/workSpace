c7df040725c9567be861ab553d5e601d4f57675d
张雨 <zhangyu@blackcrystaltech.com>
2017-09-18 10:50:48 +0800

    +补充提交

--- WOTWorkSpace/common/WOTUserSingleton.h
+++ WOTWorkSpace/common/WOTUserSingleton.h
@@ -7,25 +7,13 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "WOTLoginModel.h"
 
 @interface WOTUserSingleton : NSObject
 
-@property (nonatomic, strong) NSString *userId;
-@property (nonatomic, strong) NSString *userName;
-@property (nonatomic, strong) NSString *password;
-@property (nonatomic, strong) NSString *realName;
-@property (nonatomic, strong) NSString *companyId;
-@property (nonatomic, strong) NSString *sex;
-@property (nonatomic, strong) NSString *headPortrait;
-@property (nonatomic, strong) NSString *constellation;
-@property (nonatomic, strong) NSString *userType;
-@property (nonatomic, strong) NSString *site;
-@property (nonatomic, strong) NSString *skill;
-@property (nonatomic, strong) NSString *interests;
-@property (nonatomic, strong) NSString *industry;
-@property (nonatomic, strong) NSString *spared1;
+//用户信息
+@property (nonatomic, strong) WOTLoginModel *userInfo;
 
 +(instancetype)shareUser;
--(void)saveUserInfoToPlist:(NSDictionary *)userinfo;
--(void)setValues;
+-(void)saveUserInfoToPlistWithModel:(WOTLoginModel *)model;
 @end
